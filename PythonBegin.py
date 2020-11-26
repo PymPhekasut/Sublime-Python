c76b3bd35b7ccec625962360accee060da48214a
PymPhekasut <74609915+PymPhekasut@users.noreply.github.com>
2020-11-26 23:36:04 +1100

    Add files via upload

---
+++ PythonBegin.py
@@ -0,0 +1,13 @@
+print('Hello Wolrd')
+name = 'Pym'
+lastname = 'Phekasut'
+
+fullname = name + lastname
+print(fullname)
+
+fullname = name+' '+lastname
+print(fullname)
+
+
+print(10+5)
+print(100//3)
